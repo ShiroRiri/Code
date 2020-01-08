c73fc9c564e9d0008a9fd756a7ed55fff6336f5e
ChristopherCE <54681676+ChristopherCE@users.noreply.github.com>
2020-01-08 15:18:15 -0800

    Update Raspberry_Pi_2.py

--- Raspberry_Pi_2.py
+++ Raspberry_Pi_2.py
@@ -9,14 +9,6 @@
 # code to install libraries for each sensor                     #
 # on the pi, write in terminal                                  #
 #                                                               #
-# BNO055                                                        #
-# sudo pip3 install adafruit-circuitpython-bno055               #
-#                                                               #
-# MMA8451                                                       #
-# sudo pip3 install adafruit-circuitpython-mma8451              #
-#                                                               #     
-# TCA9548A                                                      #
-# sudo pip3 install adafruit-circuitpython-tca9548a             #
-# https://github.com/adafruit/Adafruit_CircuitPython_TCA9548A   #
+# ADS1115                                                       #
+# sudo pip3 install adafruit-circuitpython-ads1x15              #
 # ///////////////////////////////////////////////////////////////
-
