57fda7b4560e29f3b191ae83b4ec06ba6c0413fd
ChristopherCE <54681676+ChristopherCE@users.noreply.github.com>
2020-01-08 15:15:50 -0800

    Update Raspberry_Pi_2.py

--- Raspberry_Pi_2.py
+++ Raspberry_Pi_2.py
@@ -1,1 +1,22 @@
+# /////////////////////////////////////////////// #
+# Raspberry Pi 2:                                 #
+#  - DS18B20 (temp probe) [x4]                    #
+#  - strain gague [x2]                            #
+#  - ADS1115 (analog to digital converter) [x2]   #
+# /////////////////////////////////////////////// #
+
+# ///////////////////////////////////////////////////////////////
+# code to install libraries for each sensor                     #
+# on the pi, write in terminal                                  #
+#                                                               #
+# BNO055                                                        #
+# sudo pip3 install adafruit-circuitpython-bno055               #
+#                                                               #
+# MMA8451                                                       #
+# sudo pip3 install adafruit-circuitpython-mma8451              #
+#                                                               #     
+# TCA9548A                                                      #
+# sudo pip3 install adafruit-circuitpython-tca9548a             #
+# https://github.com/adafruit/Adafruit_CircuitPython_TCA9548A   #
+# ///////////////////////////////////////////////////////////////
 
