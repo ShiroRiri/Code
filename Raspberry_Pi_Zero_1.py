890815da6fe7b1e174d98a725bed6113f1d38011
joshgarde <joshgarde@gmail.com>
2020-02-23 19:23:51 -0800

    Initial board 4 rewrite

--- Raspberry_Pi_Zero_1.py
+++ Raspberry_Pi_Zero_1.py
@@ -1,105 +1,67 @@
-# /////////////////////////////////////////////// #
-# Pi Zero 1                                       #
-#  - RFM96W - 433 MHz radio transceiver           #
-#  - DS18B20 -temp probe [x1]                     #
-#  - DS18B20 -stain gauge[x1]                     #
-#  - ADS1115 - analog to digital converter [x1]   #
-# /////////////////////////////////////////////// #
-
-# ///////////////////////////////////////////////////////////////
-# code to install libraries for each sensor                     #
-#                                                               #
-# RFM699(w)  -look into this                                    #      
-# sudo pip3 install adafruit-circuitpython-rfm69                #
-#                                                               #
-# ADS1115                                                       #
-# sudo pip3 install adafruit-circuitpython-ads1x15              #
-# ///////////////////////////////////////////////////////////////
-
-
+import sys
+import threading
 import board
 import busio
-import digitalio
-import adafruit_ads1x15.ads1115 as ADS  #for temp probe ads1115
-# for the analog conversion
-import board 
-import analogio 
-adc = analogio.AnalogIn(board.A0) 
-
-
-# it states spi but we can chagnge it to i2c
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-# i2c = busio.I2C(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-# defining the boards (CS and RST pins)
-cs = digitalio.DigitalInOut(board.D5)
-reset = digitalio.DigitalInOut(board.D6)
-
-# specifying the specified--confirmed
-import adafruit_rfm69
-rfm69 = adafruit_rfm69.RFM69(spi, cs, reset, 433.0) #potential swap to I2C
-
-# example message
-rfm69.send('Test Code!')
-
-# recieveing message 
-rfm69.receive()
-
-# pause time, wait 1 second
-rfm69.receive(timeout_s=1.0)  
-
-# If we would like to convert a byte string into an ASCII text string
-#-------------------------------------------------------------------------------
-packet = rfm69.receive()  # Wait for a packet to be received (up to 0.5 seconds)
-if packet is not None:
-    packet_text = str(packet, 'ascii')
-    print('Received: {0}'.format(packet_text))
-    
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-
-# code for the ads1115
-# final import for the ADS1x15 library's version of AnalogIn
+import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ds18x20 import DS18X20
+from adafruit_onewire.bus import OneWireBus
+from bintools import DataLogWriter, FieldTypes
 
-# create the ADC object
-ads = ADS.ADS1115(i2c)
+# If script is directly called, start main
+if __name__ == "__main__":
+    main()
 
-# we can use two types of mode Single Ended or Differential 
+def main():
+    i2cWriter = DataLogWriter('~/i2c-log.bin', [{'name': 'StrainGuage', 'type': FieldTypes.FLOAT}])
+    owWriter = DataLogWriter('~/ow-log.bin', [{'name': 'TempProbe', 'type': FieldTypes.FLOAT}])
 
-# code for Single Ended mode
-chan = AnalogIn(ads, ADS.P0) 
+    initializeIO()
+    startLogging()
 
-# if we want to set up adddition channels, we keep the same syntax
-# but all we have to do is change the pin
+def initializeIO():
+    try:
+        i2c = busio.I2C(board.SCL, board.SDA)
+        ow_bus = OneWireBus(board.D4)
+        
+        # Initialize devices
+        global adc
+        global temp_sensor
+        adc = ADS.ADS1115(i2c)
+        temp_sensor = DS18X20(ow_bus, ow_bus.scan()[0])
+        temp_sensor.resolution = 9 # Speeds up the polling rate while sacrificing some resolution
 
-# print the amount of voltage
-print(chan.value, chan.voltage)
+        print("IO successfully initialized")
 
-# Differential Mode
-# we will need 20 pings for this to work
-chan = AnalogIn(ads, ADS.P0, ADS.P1)
+    except Exception as e:
+        print("Error while initializing IO - {}\n".format(e.message))
+        sys.exit(1)
 
-# getting the readings
-print(chan.value, chan.voltage)
+# Threading utilized to ensure maximum throughput on all IO buses
+def startLogging(): 
+    print("Starting logging threads...")
 
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
+    # Create logging threads
+    i2cThread = threading.Thread(target = logI2CDevices, name = 'i2c-logger')
+    owThread = threading.Thread(target = logOWDevices, name = 'ow-logger')
 
-# test value for ADC (analog to digital conversion)
-#set test value (either or)
->>> adc.value
-32683
+    # Start both threads
+    i2cThread.start()
+    owThread.start()
 
->>> adc.value
-65
+    # Ensure script doesn't exit until both are finished
+    i2cThread.join()
+    owThread.join()
 
-# reference voltage used by the ADC to convert voltages into numbers 
->>> adc.value / 65535 * adc.reference_voltage
-3.2998
+def logI2CDevices():
+    while True:
+        i2cWriter.beginSample()
+        adc_sample = AnalogIn(adc, 0, 1)
+        i2cWriter.log(adc_sample.voltage)
+        i2cWriter.endSample()
 
-# we can also make a python function to call the ADC easier
->>> def adc_to_voltage(val):
-...   return val / 65535 * 3.3
->>> adc_to_voltage(adc.value)
-3.2998
+def logOWDevices():
+    while True:
+        owWriter.beginSample()
+        owWriter.log(temp_sensor.temperature())
+        owWriter.endSample()
